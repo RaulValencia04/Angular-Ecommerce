5eb3adac5103f4c3f1ae518c62826af63a7efb10
RaulValencia04 <raulvalenciau@gmail.com>
2023-09-22 10:50:09 -0600

    navbar funcional en todo el sistema y carrito modificado

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UsersService } from "../users/users.service";
+import { CookieService } from "ngx-cookie-service"; // Importa el servicio de cookies
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +9,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+
+  username: string = "";
+  hasUser: boolean = false;
+
+  constructor(
+    private userService: UsersService,
+    private cookieService: CookieService, // Inyecta el servicio de cookies
+    private router: Router // Inyecta el servicio de enrutamiento
+  ) {}
+
+  checkUser(): void {
+    const userCookie = this.cookieService.get("user"); // Obtiene el valor de la cookie "user"
+    if (userCookie) {
+      this.username = userCookie; // Establece el nombre de usuario si existe
+      this.hasUser = true; // Indica que existe un usuario en las cookies
+    }
+  }
+  isLoggedIn(): boolean {
+    return this.cookieService.check("user"); // Devuelve true si la cookie "user" existe
+  }
+
+  // Función para obtener el nombre de usuario desde la cookie
+  getUsername(): string {
+    const userData = this.cookieService.get("user");
+    if (userData) {
+      const userObject = JSON.parse(userData);
+      return userObject.nombre_usuario || "";
+    }
+    return "";
+  }
+  logout() {
+    // Borra la cookie
+    this.cookieService.delete("user"); // Reemplaza "user" con el nombre de tu cookie
+
+
+  }
+
+  redirectToCart(): void {
+    this.router.navigate(['/carrito']); // Cambia '/carrito' a la ruta de tu página de carrito
+  }
 
 }
