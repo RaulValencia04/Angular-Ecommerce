ce86e7147bfb99e26bb22590b073994fa0045f4b
RaulValencia04 <raulvalenciau@gmail.com>
2023-09-27 12:11:33 -0600

    factura descomentada

--- src/app/facturacion/facturacion.component.ts
+++ src/app/facturacion/facturacion.component.ts
@@ -1,22 +1,35 @@
-import { Component , OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { UsersService } from "../users/users.service";
 import { Router } from "@angular/router";
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from "@angular/common/http";
 import { CookieService } from "ngx-cookie-service";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-facturacion',
   templateUrl: './facturacion.component.html',
   styleUrls: ['./facturacion.component.css']
 })
-export class FacturacionComponent {
-  factura:any[] = []
+export class FacturacionComponent implements OnInit {
+  factura: any[] = [];
+  vali: boolean = true;
 
-  constructor(public userService: UsersService, private router: Router, private http: HttpClient ,private cookieService: CookieService) {}
+  constructor(
+    public userService: UsersService,
+    private router: Router,
+    private http: HttpClient,
+    private cookieService: CookieService
+  ) {}
 
   ngOnInit(): void {
+    // Verificar si se debe realizar la recarga
+    // if (this.vali) {
+    //   // Establecer vali a falso para evitar futuras recargas
+    //   this.vali = false;
 
+    //   // Recargar la página actual
+    //   window.location.reload();
+    // }
 
     const user = this.getIdUsuarioFromCookie();
 
@@ -26,34 +39,31 @@ export class FacturacionComponent {
         if (data) {
           this.factura = data;
         } else {
-          console.log("si llega aqui inserto datos");
+          console.log("si llega aquí inserto datos");
         }
       },
       (error) => {
         console.log(error);
       }
     );
-
-
-
   }
+
   imprimirFactura() {
     window.print();
   }
+
   getIdUsuarioFromCookie(): number {
-    const userData = this.cookieService.get('user');
+    const userData = this.cookieService.get("user");
     if (userData) {
       const userObject = JSON.parse(userData);
-      // console.log(userObject.id_usuario+ ''+ typeof userObject.id_usuario)
-      return userObject.id_usuario || '';
+      return userObject.id_usuario || 1;
     }
 
     return 1; // Ejemplo: reemplaza esto con la lógica real
   }
-  deleteCompras(){
+
+  deleteCompras() {
     const user = this.getIdUsuarioFromCookie();
-
-
 
     try {
       this.userService.EliminarPedido(user).subscribe(
@@ -62,27 +72,21 @@ export class FacturacionComponent {
           // this.loadCarritoProductos(idUsuarioFromCookie);
         },
         (error) => {
-          console.error('Error al eliminar el carrito:', error);
+          console.error("Error al eliminar el carrito:", error);
         }
       );
       this.router.navigateByUrl("/");
-
     } catch (error) {
-       console.log("algo anda mal. "+error)
-
+      console.log("Algo anda mal. " + error);
     }
-
-
-
-
   }
+
   mostrarAlerta() {
     Swal.fire({
       title: 'Compra finalizada :3',
-      icon: 'success', // Puedes cambiar el ícono a tu gusto (success, error, warning, etc.)
-      timer: 1500, // Tiempo de visualización en milisegundos (2 segundos)
-      showConfirmButton: false // Oculta el botón de confirmación
+      icon: 'success',
+      timer: 1500,
+      showConfirmButton: false
     });
   }
-
 }
